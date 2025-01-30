4788539b2828012bd3dbe347e216fd5026dbcbb5
qaq_fei <qaq_fei@163.com>
2025-01-30 15:41:35 +0800

    fix bug

--- prpr/src/scene/ending.rs
+++ prpr/src/scene/ending.rs
@@ -14,11 +14,10 @@ use crate::{
 };
 use anyhow::Result;
 use macroquad::prelude::*;
-use rand::Rng;
 use sasa::{AudioClip, AudioManager, Music, MusicParams};
 use serde::Deserialize;
 use std::{cell::RefCell, ops::DerefMut};
-use ::rand::{ thread_rng, Rng as _ };
+use ::rand::{thread_rng, Rng as _};
 
 #[derive(Deserialize)]
 pub struct RecordUpdateState {
@@ -320,7 +319,7 @@ impl Scene for EndingScene {
             });
 
             if let Some(s) = &self.update_state {
-                let mut rng = rand::thread_rng();
+                let mut rng = thread_rng();
                 let mut imp: u32 = rng.gen_range(345..1000) as u32;
                 if s.best {
                     ui.text(format!("{}  {:+07}", tl!("new-best"), imp))
@@ -353,7 +352,7 @@ impl Scene for EndingScene {
             let r = ui.text("|").pos(r.right() + 0.03, r.y).color(cs).size(s).draw();
 
             let r = ui.text(tl!("error")).pos(r.right() + 0.03, r.y).color(cl).size(s).draw_using(&BOLD_FONT);
-            let mut rng = rand::thread_rng();
+            let mut rng = thread_rng();
             ui.text(format!("±{}ms", rng.gen_range(7..29) as i32))
                 .pos(r.right() + 0.02, r.y)
                 .size(s)
